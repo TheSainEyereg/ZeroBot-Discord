a1cd54822cdc4c9d127fb148c83b41b208fc83c0
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2021-08-09 23:03:54 +0300

    Fixed argument display

--- commands/misc/avatar.js
+++ commands/misc/avatar.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: "avatar",
     aliases: ["ava", "profile"],
 	description: "Sends user avatar (1024x1024)",
-	arguments: ["[user]"],
+	arguments: ["(user)"],
     optional: true,
 	async execute(message, args) {
         const user = message.mentions.users.first() || message.author;
