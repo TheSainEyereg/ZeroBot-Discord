9687d3161fcb1ccc96ca77af82dfd0676638d6fe
Oleg Logvinov <37226226+TheSainEyereg@users.noreply.github.com>
2021-09-18 14:36:55 +0300

    Update example.js

--- commands/ignore/example.js
+++ commands/ignore/example.js
@@ -1,14 +1,14 @@
-const Messages = require("../../core/Messages");
+const Messages = require("../../core/Messages"); // Requires "Messages" core for cool looking messages
 
-module.exports = {
-	name: "example", //Command name
-    aliases: ["test", "sample"], //Example can also be called with "test" or "sample" 
-    description: "Example command", //Displays in help command
-    arguments: ["(Something)"], //Displays in help command and also uses for command execution check
-    optional: true, //Is arguments optional?
-    access: ["user", "dj", "moderator", "administrator", "owner", "superuser"], //Who can execute this command
-	execute(message, args) { //Main function (Dont change function args pls ;))
-        if (!args[0]) return Messages.warning(message, "No args but anyway Hello ;)");
-        Messages.complete(message, args[0]);
-	}
-};
+module.exports = { // Opens command module
+	name: "example", // Command name
+	aliases: ["test", "sample"], // Example can also be called with "test" or "sample" 
+	description: "Example command", // Displays in help command
+	arguments: ["(Something)"], // Displays in help command and also uses for command execution check
+	optional: true, // Is arguments optional?
+	access: ["user", "dj", "moderator", "administrator", "owner", "superuser"], // Who can execute this command
+	execute(message, args) { // Main function ( Dont change function args pls ;) )
+		if (!args[0]) return Messages.warning(message, "No args but anyway Hello ;)"); // Checks if first argument is present, if not, It sends warning message
+		Messages.complete(message, args[0]); // If arguments present it repeats first argument in message (Like something is completed)
+	} // Closes main function
+}; // Closes command module
