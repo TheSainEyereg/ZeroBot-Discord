169149ccac01b778e1dfda461e45f1c918e39bc8
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2021-09-18 12:23:56 +0300

    Make prefix command look better

--- commands/settings/prefix.js
+++ commands/settings/prefix.js
@@ -12,6 +12,6 @@ module.exports = {
 		if (args[0].length > 3) return Messages.warning(message,"Max prefix length is 3!");
         Servers.set(message.guild.id, "prefix", args[0]);
         Logs.regular(__filename, `User ${message.author.id} (${message.author.tag}) changed prefix for server ${message.guild.id} to "${args[0]}"`);
-        Messages.complete(message, `Successfuly changed prefix to "${args[0]}"`);
+        Messages.complete(message, `Successfuly changed prefix to \`${args[0]}\``);
 	}
 };
