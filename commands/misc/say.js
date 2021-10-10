e4de65f9aefc4a992c31472d2994a25c1596c162
TheSainEyereg <oleglogwinow@gmail.com>
2021-10-10 11:56:13 +0300

    Update say.js

--- commands/misc/say.js
+++ commands/misc/say.js
@@ -6,10 +6,7 @@ module.exports = {
 	aliases: ["echo"],
 	execute(message, args) {
 		const l = Localization.server(message.client, message.guild, this.name);
-		let out = args.join(" ");
-		const re = /@|.:\/\/|https:\/\/|http:\/\/|ftp:\/\//gi;
-		while (out.match(re)) out = out.replace(re,"");
-		if (!out) return Messages.warning(message, l.error);
-		Messages.advanced(message, out, 0, {custom:`By ${message.author.tag}`, noicon:true});
+		if (!args.join(" ")) return Messages.warning(message, l.error);
+		Messages.advanced(message, 0, args.join(" "), {custom:`By ${message.author.tag}`, noicon:true});
 	}
 };
