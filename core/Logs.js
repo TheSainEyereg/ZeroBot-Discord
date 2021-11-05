b2835d092a039d500ec09c68e5730efdec62a8f0
TheSainEyereg <oleglogwinow@gmail.com>
2021-11-05 12:17:33 +0300

    Added JSDoc to Logs core

--- core/Logs.js
+++ core/Logs.js
@@ -2,10 +2,22 @@ const fs = require("fs");
 
 module.exports = {
     checkDir() {!fs.existsSync("./logs") ? fs.mkdirSync("./logs") : null},
+	/** 
+	 * Returns the current date in the format YY-MM-DD-HH:MM:SS
+	 * @returns {String}
+	*/
     getDateString() {
         const date = new Date;
         return `${("0"+date.getFullYear().toString().slice(2)).slice(-2)}.${("0"+(date.getMonth()+1)).slice(-2)}.${("0"+date.getDate()).slice(-2)}-${("0"+date.getHours()).slice(-2)}:${("0"+date.getMinutes()).slice(-2)}:${("0"+date.getSeconds()).slice(-2)}:${("0"+date.getMilliseconds()).slice(-3)}`;
     },
+	/**
+	 * Writes regular logs to the log file
+	 * @param {String} source - The source of the log
+	 * @param {String} text - The text to be written
+	 * @param {Object} [opt] - Optional options
+	 * @param {String} opt.pre - Prefix to be added to the log
+	 * @param {String} opt.nonl - If set to true, the log will not be followed by a newline
+	*/
     async regular(source, text, opt) {
         this.checkDir();
         if (!fs.existsSync("./logs/regular.log")) {
@@ -18,6 +30,14 @@ module.exports = {
         out+=`${text}${opt && opt.nonl ? "" : "\n"}`;
         fs.appendFileSync("./logs/regular.log", out);
     },
+	/**
+	 * Writes criticals logs to the log file
+	 * @param {String} source - The source of the log
+	 * @param {String} text - The text to be written
+	 * @param {Object} [opt] - Optional options
+	 * @param {String} opt.pre - Prefix to be added to the log
+	 * @param {String} opt.nonl - If set to true, the log will not be followed by a newline
+	*/
     async critical(source, text, opt) {
         this.checkDir();
         if (!fs.existsSync("./logs/critical.log")) {
@@ -30,6 +50,15 @@ module.exports = {
         out+=`${text}${opt && opt.nonl ? "" : "\n"}`;
         fs.appendFileSync("./logs/critical.log", out);
     },
+	
+	/**
+	 * Writes security logs to the log file
+	 * @param {String} source - The source of the log
+	 * @param {String} text - The text to be written
+	 * @param {Object} [opt] - Optional options
+	 * @param {String} opt.pre - Prefix to be added to the log
+	 * @param {String} opt.nonl - If set to true, the log will not be followed by a newline
+	*/
     async security(source, text, opt) {
         this.checkDir();
         if (!fs.existsSync("./logs/security.log")) {
