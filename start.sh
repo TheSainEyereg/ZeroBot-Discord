eb55503c2f5d0dd46e4acd552f6d1c5247154625
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2022-01-27 11:59:41 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,14 @@ echo "Starting discord bot. You can view bot screen, type screen -r zerobot."
 echo "To exit from screen, press Ctrl+A then Ctrl+D."
 cd /home/ubuntu/ZeroBot-Discord
 
-FILE_NAME="stdout_"$(date +"%Y.%m.%d-%H:%M:%S")".log"
+STDOUT_FILE_NAME="stdout_"$(date +"%Y.%m.%d-%H:%M:%S")".log"
+STDERR_FILE_NAME="stderr_"$(date +"%Y.%m.%d-%H:%M:%S")".log"
 
 if [ -f "logs/stdout_latest.log" ]; then
-	mv logs/stdout_latest.log logs/$FILE_NAME
+	mv logs/stdout_latest.log logs/$STDOUT_FILE_NAME
+fi
+if [ -f "logs/stderr_latest.log" ]; then
+	mv logs/stderr_latest.log logs/$STDERR_FILE_NAME
 fi
 
-/bin/screen -dmS zerobot bash -c "/home/ubuntu/.nvm/versions/node/v17.0.1/bin/node index.js | tee logs/stdout_latest.log"
+/bin/screen -dmS zerobot bash -c "/home/ubuntu/.nvm/versions/node/v17.0.1/bin/node index.js 1> >(tee logs/stdout_latest.log) 2> >(tee logs/stderr_latest.log) >&2"
