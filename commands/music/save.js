550481d1c859b03ee4452c562c7508d6552aab5a
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2022-02-18 21:23:37 +0300

    Added "save" command

---
+++ commands/music/save.js
@@ -0,0 +1,29 @@
+const Localization = require("../../core/Localization");
+const Messages = require("../../core/Messages");
+const {MessageEmbed} = require("discord.js");
+
+module.exports = {
+	name: "save",
+	description: "Send current playing song to DM",
+	aliases: ["s", "like", "favorite", "fav"],
+	async execute(message, args) {
+		const l = Localization.server(message.client, message.guild, this.name);
+		const queue = message.client.queue.get(message.guild.id);
+		if (!queue) return Messages.warning(message, l.nothing);
+
+		// Check if the user has DM's enabled
+		const DMChannel = await message.author.createDM();
+		if (!DMChannel) return Messages.warning(message, l.dm_disabled);
+
+		const song = queue.list[0];
+		DMChannel.send({embeds:[new MessageEmbed({
+			color: Messages.colors.regular,
+			thumbnail: {
+				url:song.thumbnail
+			},
+			title: song.title,
+			url: song.url
+		})]});
+		Messages.success(message, l.sent);
+	}
+}
