8f0ebec95aec4ac9640002dfb00d4927e5224ed7
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2022-05-14 20:28:20 +0300

    Update ascii.js

--- commands/misc/ascii.js
+++ commands/misc/ascii.js
@@ -6,13 +6,13 @@ module.exports = {
 	name: "ascii",
 	execute(message, args) {
 		const l = Localization.server(message.client, message.guild, this.name);
-		axios.get(`https://artii.herokuapp.com/make?text=${args.join(" ")}`)
+		axios.get(`https://api.olejka.ru/v2/figlet?text=${args.join(" ")}`)
 		.then(res => {
-			message.channel.send(`\`\`\`${res.data}\`\`\``);
+			message.channel.send(`\`\`\`\n${res.data.text}\`\`\``);
 		})
 		.catch(e => {
-			Logs.critical(`${this.name} command`, `Error in ascii converting: ${e}`);
-			return Messages.critical(message, `${l.error}: \`\`\`${e}\`\`\``);
+			Logs.critical(`${this.name} command`, `Error in ascii convertation: ${e}`);
+			return Messages.critical(message, `${l.error}: \`\`\`${e.response?.data?.error || e}\`\`\``);
 		});
 	}
 };
