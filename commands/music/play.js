b089adf57e6b619ef320a1cd0124ce97a314f239
TheSainEyereg <37226226+TheSainEyereg@users.noreply.github.com>
2022-05-23 20:06:43 +0300

    Added useragent

--- commands/music/play.js
+++ commands/music/play.js
@@ -207,6 +207,7 @@ module.exports = {
 		}
 
 		play.setToken({
+			useragent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36",
 			soundcloud: {
 				clientId: config.SCClient || (await play.getFreeClientID())
 			}
