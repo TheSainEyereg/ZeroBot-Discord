da9dfef00dda3568d01ff289f14a00362be057a2
TheSainEyereg <oleglogwinow@gmail.com>
2023-06-17 21:01:31 +0300

    Deleted everything

--- commands/info/ping.js
+++
@@ -1,8 +0,0 @@
-const Messages = require("../../components/Messages");
-
-module.exports = {
-	name: "ping",
-	execute(message, args) {
-		Messages.regular(message, `Pong! ${Date.now()-message.createdTimestamp}ms.`, {big: true})
-	}
-};
