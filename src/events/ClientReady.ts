5178eeec9851472648dff81bbf18d9ba17cd16bb
TheSainEyereg <oleglogwinow@gmail.com>
2023-09-28 17:28:22 +0300

    WTF

--- src/events/ClientReady.ts
+++ src/events/ClientReady.ts
@@ -7,10 +7,17 @@ export default class Ready extends Event {
 
 	execute = async (client: Client) => {
 		console.log(`Ready! Logged in as ${client.user!.tag}`);
-		
+
 		// Merge commands with duplicate names
 		const commandsJSON = client.commands.filter(cmd => cmd.data !== null).map(cmd => cmd.data!.toJSON());
-		commandsJSON.forEach((cmd, index) => commandsJSON.forEach((c, i) => (c.name === cmd.name && i !== index) && commandsJSON.splice(i, 1) && (cmd.options = [... cmd.options!, ... c.options!])));
+		for (const cmd of commandsJSON) {
+			for (const c of commandsJSON) {
+				if (c.name === cmd.name && c !== cmd) {
+					commandsJSON.splice(commandsJSON.indexOf(c), 1);
+					cmd.options = [...cmd.options!, ...c.options!];
+				}
+			}
+		}
 
 		//await client.application!.commands.set([]);
 		await client.application!.commands.set(commandsJSON);
