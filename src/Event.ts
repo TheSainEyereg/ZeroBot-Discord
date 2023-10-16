037fb0b73ec9cd5c461782a8886a842abae3dab9
TheSainEyereg <oleglogwinow@gmail.com>
2023-10-17 00:01:50 +0300

    Update Event.ts

--- src/Event.ts
+++ src/Event.ts
@@ -1,5 +1,5 @@
 export default abstract class Event {
 	abstract event: string;
-	abstract execute(args?: unknown): void;
+	abstract execute(...args: (unknown | undefined)[]): void;
 	once = false;
 }
